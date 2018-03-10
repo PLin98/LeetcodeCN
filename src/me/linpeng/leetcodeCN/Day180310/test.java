c7bb6f9c96c7184538b120a6124282f4ccd8c1a3
PLin98 <rin98@qq.com>
2018-03-10 15:13:30 +0800

    test180310

---
+++ src/me/linpeng/leetcodeCN/Day180310/test.java
@@ -0,0 +1,8 @@
+package me.linpeng.leetcodeCN.Day180310;
+
+public class test {
+	public static void main(String[] args) {
+	Solution1 s=new Solution1();
+	System.out.println(s.reverse(214740000));
+	}
+}
