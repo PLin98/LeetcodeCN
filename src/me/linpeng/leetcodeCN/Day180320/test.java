326fbf8a3fb8c7fdde6f8f90a5c4505e22b573f0
PLin98 <rin98@qq.com>
2018-03-20 15:42:26 +0800

    finish

---
+++ src/me/linpeng/leetcodeCN/Day180320/test.java
@@ -0,0 +1,12 @@
+package me.linpeng.leetcodeCN.Day180320;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Solution solution=new Solution();
+		int n=4;
+		System.out.println(solution.countAndSay(n));
+	}
+
+}
