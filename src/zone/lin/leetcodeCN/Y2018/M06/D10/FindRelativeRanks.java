ce3f9be59ed63f24eac42e81c16c8da2f4a6eec4
rin <rin98@qq.com>
2018-06-10 13:59:20 +0800

    finish

---
+++ src/zone/lin/leetcodeCN/Y2018/M06/D10/FindRelativeRanks.java
@@ -0,0 +1,42 @@
+/*给出 N 名运动员的成绩，找出他们的相对名次并授予前三名对应的奖牌。前三名运动员将会被分别授予 “金牌”，“银牌” 和“ 铜牌”（"Gold Medal", "Silver Medal", "Bronze Medal"）。
+(注：分数越高的选手，排名越靠前。)
+示例 1:
+输入: [5, 4, 3, 2, 1]
+输出: ["Gold Medal", "Silver Medal", "Bronze Medal", "4", "5"]
+解释: 前三名运动员的成绩为前三高的，因此将会分别被授予 “金牌”，“银牌”和“铜牌” ("Gold Medal", "Silver Medal" and "Bronze Medal").
+余下的两名运动员，我们只需要通过他们的成绩计算将其相对名次即可。
+提示:
+N 是一个正整数并且不会超过 10000。
+所有运动员的成绩都不相同。*/
+package zone.lin.leetcodeCN.Y2018.M06.D10;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+class Solution {
+    public String[] findRelativeRanks(int[] nums) {
+        int[] num=new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        	num[i]=nums[i];
+        Arrays.sort(num);//将成绩排序
+        int i=1;
+        HashMap<Integer,Integer> map=new HashMap<Integer,Integer>();
+        for(int j=num.length-1;j>=0;j--) {
+        	map.put(num[j], i);
+        	i++;
+        }
+        String[] res=new String[nums.length];
+        for(int j=0;j<nums.length;j++) {
+        	int temp=map.get(nums[j]);//获得排名
+        	if(temp==1)
+        		res[j]="Gold Medal";
+        	else if(temp==2)
+        		res[j]="Silver Medal";
+        	else if(temp==3)
+        		res[j]="Bronze Medal";
+        	else
+        		res[j]=""+temp;
+        }
+        return res;
+    }
+}
